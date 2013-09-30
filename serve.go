fd1d7cd239276857d24b9c01faa79fe15f86899a
Luke Evers <me@lukevers.com>
2013-09-30 12:43:32 -0400

    Initial Commit of serve.go

---
+++ serve.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+	"html/templates"
+)
+
+var (
+	t *template.Template
+)
+
+// Serve starts the HTTP server
+func Serve(config *Config) {
+	// Start the API if enabled
+	if config.Access.EnableAPI {
+		l.Info("Starting API")
+		// TODO
+	}
+	
+	// Start the front-end if enabled
+	if config.Access.EnableFrontEnd {
+		l.Info("Starting front-end")
+		http.HandleFunc("/", HandleRoot)
+		http.HandleFunc("/assets", HandleAssets)
+		// Compile templates
+		t = template.Must(template.ParseGlob("templates/*.html"))
+	}
+
+	// Listen and serve
+	http.ListenAndServe(config.Address, nil)
+}
+
+// HandleAssets is a static file server that serves everything in
+// static directory
+func HandleAssets(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+// HandleRoot 
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	
+}
