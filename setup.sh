7b87c6b0b7f06f042bcd0612e581ca26c4224e8d
Luke Evers <me@lukevers.com>
2013-10-02 09:18:03 -0400

    Made the setup script more user-friendly

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 for i in "googlefontdirectory/fonts/*.ttf"
 do 
-    convert.pe --format ".woff" $i
-    convert.pe --format ".otf" $i
-    convert.pe --format ".eot" $i
-    convert.pe --format ".svg" $i
+    ./convert.pe --format ".woff" $i
+    ./convert.pe --format ".otf" $i
+    ./convert.pe --format ".eot" $i
+    ./convert.pe --format ".svg" $i
 done
